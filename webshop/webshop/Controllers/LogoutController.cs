4b49b5466481e4b530db8c8f3585cf64fca54b89
Bálint Varga <vargabalint051@gmail.com>
2025-02-17 10:42:03 +0100

    Created Logout endpoint

---
+++ webshop/webshop/Controllers/LogoutController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webshop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogoutController : ControllerBase
+    {
+        [HttpPost("{token}")]
+        public IActionResult Logout(string token)
+        {
+            if (Manager.LoggedInUsers.ContainsKey(token))
+            {
+                Manager.LoggedInUsers.Remove(token);
+                return Ok("Sikeres kijelentkeztetés!");
+            }
+            else return NotFound("Felhasználó nem található!");
+        }
+    }
+}
--- webshop/webshop/obj/Release/net8.0/webshop.AssemblyInfo.cs
+++ webshop/webshop/obj/Release/net8.0/webshop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webshop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51e83ff7c2970665c5816a77651b059dbc8ac612")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2f016ce58a496ade3b92e2fe07027585cea7439")]
 [assembly: System.Reflection.AssemblyProductAttribute("webshop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webshop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
