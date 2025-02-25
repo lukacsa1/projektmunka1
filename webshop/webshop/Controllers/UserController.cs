faa13e4afb10709b4a5a169946ad51bef39c413f
Bálint Varga <vargabalint051@gmail.com>
2025-02-25 11:05:34 +0100

    Created UserController and GetAllUsers endpoint

---
+++ webshop/webshop/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using webshop.Models;
+
+namespace webshop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("GetAll")]
+        public IActionResult GetAllUsers(string token)
+        {
+            if(Manager.CheckPermission(token, 9))
+            {
+                using (var context = new WebshopContext())
+                {
+                    try
+                    {
+                        List<User> users = context.Users.ToList();
+                        return Ok(users);
+                    }
+                    catch (Exception ex)
+                    {
+                        return BadRequest("Nem sikerült lekérni a felhasználókat! " + ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                return Unauthorized(Manager.UserNotEligableMessage);
+            }
+        }
+    }
+}
--- webshop/webshop/obj/Debug/net8.0/webshop.AssemblyInfo.cs
+++ webshop/webshop/obj/Debug/net8.0/webshop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webshop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35800780a405f5f303d0f915414461c502240b4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdc8de5e126d34b1da739d08c0c88eb0596585d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("webshop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webshop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
